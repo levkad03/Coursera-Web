202135b6f7e3ad25c88d9992499929c4f7db04cc
Lev Kadatskyi <levkadatscky@gmail.com>
2024-03-18 15:31:25 +0200

    added navbar collapse on blur

---
+++ restaurant/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".navbar-toggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse("hide");
+    }
+  });
+});
